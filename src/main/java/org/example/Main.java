98075a47d819456d9a9df405ce403410414d7b22
sandaruanjana <sandaruanjana@outlook.com>
2023-07-29 15:50:08 +0530

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * @author Sandaru Anjana <sandaruanjana@outlook.com>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
